cf257ac0027c91d487f149f1377796797f96996f
Marcos Paulo <pmarcos4235@gmail.com>
2023-03-09 16:44:09 -0300

    Update express.js

--- src/modules/express.js
+++ src/modules/express.js
@@ -10,10 +10,10 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 
 app.get('/static/js/main.16a63006.js', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/static/js/main.16a63006.js'))
+    res.sendFile(path.join(__dirname, '../public/Static/js/main.16a63006.js'))
 })
 app.get('/static/css/main.4f6fd11c.css', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/static/css/main.4f6fd11c.css'))
+    res.sendFile(path.join(__dirname, '../public/Static/css/main.4f6fd11c.css'))
 })
 
 
