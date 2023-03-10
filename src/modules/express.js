1046ec730cdcde283d70d27eae00e6d71b74c564
Marcos Paulo <pmarcos4235@gmail.com>
2023-03-10 07:08:15 -0300

    Update express.js

--- src/modules/express.js
+++ src/modules/express.js
@@ -16,10 +16,4 @@ app.get('/destaqueMenuHome', async (req, res) => {
     res.json(await DMH.getData())
 })
 
-// app.get('/static/js/main.16a63006.js', async (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/Static/js/main.16a63006.js'))
-// })
-// app.get('/static/css/main.4f6fd11c.css', async (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/Static/css/main.4f6fd11c.css'))
-// })
 app.listen(port, () => console.log(`porta: ${port} aberta`))
