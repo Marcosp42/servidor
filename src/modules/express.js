6052ebca22c8199b926884186c7589e18bacb5e9
Marcos Paulo <pmarcos4235@gmail.com>
2023-03-10 06:40:44 -0300

    Update express.js

--- src/modules/express.js
+++ src/modules/express.js
@@ -6,16 +6,16 @@ const app = express()
 const port = process.env.PORT || 3001;
 
 app.use(urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, '../public')))
+const publicFolder = path.normalize(path.join(__dirname, '../public'))
+app.use(express.static(publicFolder))
 
 app.get('/destaqueMenuHome', async (req, res) => {
     res.json(await DMH.getData())
-    res.end()
 })
-app.get('/static/js/main.16a63006.js', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/Static/js/main.16a63006.js'))
-})
-app.get('/static/css/main.4f6fd11c.css', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/Static/css/main.4f6fd11c.css'))
-})
+// app.get('/static/js/main.16a63006.js', async (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/Static/js/main.16a63006.js'))
+// })
+// app.get('/static/css/main.4f6fd11c.css', async (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/Static/css/main.4f6fd11c.css'))
+// })
 app.listen(port, () => console.log(`porta: ${port} aberta`))
