f22a4c5e5e9d32affa1871c3a8b733fb97099fea
Marcos Paulo <95592629+Marcosp42@users.noreply.github.com>
2023-03-10 13:25:58 -0300

    Add files via upload

--- src/modules/express.js
+++ src/modules/express.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const DMH = require('../model/destaqueMenu')
 const app = express()
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8080;
 
 app.use(urlencoded({ extended: true }))
 
